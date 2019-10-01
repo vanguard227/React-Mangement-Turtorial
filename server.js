3679a5f1004f36696796caf1c2cf89f9aa51c162
Vanguard <vanguard227@protonmail.com>
2019-10-01 18:54:52 +0800

    Create customer Database Table & Node.js Database Connection Setting

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
@@ -6,33 +7,26 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true} ));
 
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: conf.host,
+    user: conf.user,
+    password: conf.password,
+    port: conf.port,
+    database: conf.database
+});
+connection.connect();
+
 app.get('/api/customers', (req, res) => {
-    res.send([
-        {
-            'id': 1,
-            'image': 'https://placeimg.com/64/64/1',
-            'name': 'HongGilDong',
-            'birthday': '961222',
-            'gender': 'Man',
-            'job': 'Student'
-        },
-        {
-            'id': 2,
-            'image': 'https://placeimg.com/64/64/2',
-            'name': 'NaYuBin',
-            'birthday': '990203',
-            'gender': 'Woman',
-            'job': 'Designer'
-        },
-        {
-            'id': 3,
-            'image': 'https://placeimg.com/64/64/3',
-            'name': 'KimLeeUn',
-            'birthday': '980806',
-            'gender': 'Man',
-            'job': 'Programmer'
+    connection.query(
+        "SELECT * FROM customer",
+        (err, rows, field) => {
+            res.send(rows);
         }
-      ]);
+    );
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`))
